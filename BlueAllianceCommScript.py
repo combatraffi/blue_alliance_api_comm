fc92f0e6af93100506b37c846fe9d6e98399e262
Dan K <kuchandw@gmail.com>
2019-03-11 23:28:06 -0700

    Some Updates and Tests

--- BlueAllianceCommScript.py
+++ BlueAllianceCommScript.py
@@ -1,20 +1,14 @@
-import requests
-import json
+import requests as rq
+import json as js
 
 
 year = 2013
 complete_match_list = []
-def how_many_matches(datain):
-    cracked_data = returned_data.split(',')
-    for i in cracked_data:
-        print(i)
-        print()
-
 
 for year in range(2013, 2019):
     auth = {'X-TBA-Auth-Key': 'Sw8UAmxSFvOgQPblLyvgMCOqzZzm2cCvXdJp1qo1xM8AWHG12WhfUR29MHZW3rLI'}
     id_header = 'frc4469:api_testing_app:v01'
-    response = requests.get('https://www.thebluealliance.com/api/v3/team/frc4469/matches/' + str(year), headers = auth)
+    response = rq.get('https://www.thebluealliance.com/api/v3/team/frc4469/matches/' + str(year), headers = auth)
     returned_data = str(response.content).split('match_number')
     del returned_data[0]
     match_list = [x[3:x.index(',')] for x in returned_data]
